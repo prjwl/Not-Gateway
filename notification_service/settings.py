d54c52e76afe88d6f913ff8a378652b3f4d053cb
Prajwal S <prajwalsandinje@gmail.com>
2023-04-25 21:55:38 +0530

    database setup

--- notification_service/settings.py
+++ notification_service/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = 'notification_service.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'nrms',
+       'USER': 'dev',
+       'PASSWORD': 'dev@123',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
+
 
 
 # Password validation
