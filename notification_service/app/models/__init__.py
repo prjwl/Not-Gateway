f099230adc74c0979b4ab007b9e078c8515d9c3d
Prajwal S <prajwalsandinje@gmail.com>
2023-04-28 21:24:43 +0530

    Created package

---
+++ notification_service/app/models/__init__.py
@@ -0,0 +1,9 @@
+from .email import Email
+from .email_config import EmailConfig
+from .email_subscribers import EmailSubscriber
+
+__all__ = [
+    'Email',
+    'EmailConfig',
+    'EmailSubscriber'
+]
