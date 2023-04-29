0e19243d70a50e8d6aede80ac9059cf44f964d52
Prajwal S <prajwalsandinje@gmail.com>
2023-04-29 21:20:28 +0530

    Integrated celery

--- notification_service/settings.py
+++ notification_service/settings.py
@@ -139,3 +139,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+CELERY_BROKER_URL = ""
+CELERY_RESULT_BACKEND = ""
