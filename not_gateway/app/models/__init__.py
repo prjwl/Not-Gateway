8d8bd11e161f1bf084df4627c1917a765a2d534b
Prajwal S <58059411+prjwl@users.noreply.github.com>
2023-08-06 10:47:36 +0530

    Added basic models for email related notifications

--- not_gateway/app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .email import Email
-from .email_config import EmailConfig
-from .email_subscribers import EmailSubscriber
-
-__all__ = [
-    'Email',
-    'EmailConfig',
-    'EmailSubscriber'
-]
